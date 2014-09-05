2eb221cbde94efb868e9bbe07da475f3d82bd380
parasquid <tristan.gomez@gmail.com>
2014-09-05 12:20:18 +0800

    fix incorrect 1.9 translation of named parameters

--- lib/http/exceptions/http_exception.rb
+++ lib/http/exceptions/http_exception.rb
@@ -3,9 +3,9 @@ module Http
     class HttpException < RuntimeError
       attr_reader :response, :original_exception
 
-      def initialize(original_exception=nil, response=nil)
-        @response = response
-        @original_exception = original_exception
+      def initialize(options = {})
+        original_exception = options[:original_exception]
+        response = options[:response]
         msg = "An error as occured while processing response."
         msg += " Status #{response.code}\n#{response.body}" if response
         msg += " Original Exception: #{original_exception}" if original_exception
